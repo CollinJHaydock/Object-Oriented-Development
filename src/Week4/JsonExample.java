a1a31782b6457d88a60c59868f197a46d19f01d9
CollinHaydock <collin.haydock@angelodevco.com>
2022-06-03 17:53:05 -0600

    Adding week 4 - I'm so lost.

---
+++ src/Week4/JsonExample.java
@@ -0,0 +1,46 @@
+package Week4;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+public class JsonExample {
+
+    public static String raiderToJson (SunwellRaiders sunwellRaiders) {
+
+        ObjectMapper mapper = new ObjectMapper();
+        String s = "";
+
+        try {
+            s = mapper.writeValueAsString(sunwellRaiders);
+        } catch (JsonProcessingException e) {
+            System.err.println(e.toString());
+        }
+        return s;
+    }
+
+    public static SunwellRaiders JsonToRaider(String s) {
+
+        ObjectMapper mapper = new ObjectMapper();
+        SunwellRaiders raider = null;
+
+        try {
+            raider = mapper.readValue(s, SunwellRaiders.class);
+        } catch (JsonProcessingException e) {
+            System.err.println(e.toString());
+        }
+
+
+        return raider;
+    }
+
+    public static void main(String[] args) {
+
+        SunwellRaiders raider1 = new SunwellRaiders();
+        raider1.setSpec("Protection");
+        raider1.setToon("Cytochrome");
+        raider1.setType("Warrior");
+        String json = JsonExample.raiderToJson(raider1);
+        System.out.println(json);
+
+        SunwellRaiders raider2 = JsonExample.JsonToRaider(json);
+        System.out.println(raider2);
+    }
+}
---
+++ src/Week4/RaiderRequestPayload.java
@@ -0,0 +1,12 @@
+package Week4;
+
+public class RaiderRequestPayload {
+    private String toon;
+
+    public String getToon() {
+        return toon;
+    }
+    public void setToon(String toon) {
+        this.toon = toon;
+    }
+}
